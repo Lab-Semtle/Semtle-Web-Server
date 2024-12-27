715cfc5c16112f0ee6d6624a79ae8633823eb0db
Leeforgiveness <grang0224@g.kmou.ac.kr>
2024-12-27 17:55:30 +0900

    feat: 프로젝트 타입 테이블 생성, builder패턴 적용

--- src/main/java/com/archisemtle/semtlewebserverspring/domain/ProjectTypeCategory.java
+++ src/main/java/com/archisemtle/semtlewebserverspring/domain/ProjectTypeCategory.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,4 +21,10 @@ public class ProjectTypeCategory {
     private Long id;
     @Column(name = "project_type_category_name")
     private String name;
+
+    @Builder
+    public ProjectTypeCategory(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }

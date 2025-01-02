ad5bc076769afbb21312fab3b464220ed64764de
Leeforgiveness <grang0224@g.kmou.ac.kr>
2025-01-02 17:02:14 +0900

    feat: 관련필드, 프로젝트 게시물 중간 repository 생성(#11)

---
+++ src/main/java/com/archisemtle/semtlewebserverspring/infrastructure/RelationFieldProjectPostMiddleRepository.java
@@ -0,0 +1,9 @@
+package com.archisemtle.semtlewebserverspring.infrastructure;
+
+import com.archisemtle.semtlewebserverspring.domain.RelationFieldProjectPostMiddle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RelationFieldProjectPostMiddleRepository extends
+    JpaRepository<RelationFieldProjectPostMiddle, Long> {
+
+}

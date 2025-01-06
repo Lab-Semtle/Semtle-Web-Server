a4e7540a3feff31d879e1ef7cb928fe48d91e502
hhjkhhbgkugjnbj <ck8901ck@gmail.com>
2025-01-06 18:40:29 +0900

    feat 개인정보수정, 개인정보조회 기능 구현

--- src/main/java/com/archisemtle/semtlewebserverspring/presentation/MemberController.java
+++ src/main/java/com/archisemtle/semtlewebserverspring/presentation/MemberController.java
@@ -5,6 +5,10 @@ import com.archisemtle.semtlewebserverspring.domain.Member;
 import com.archisemtle.semtlewebserverspring.dto.MemberRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +25,30 @@ public class MemberController {
     public Member save(@RequestBody MemberRequestDto memberRequestDto) {
         memberService.save(memberRequestDto);
         return null;
+    }
+
+    // 개인 정보 조회
+    // 입력 - member_id
+    // 출력 - 이름, 생년월일, 전화번호, 학번, 프로필 사진
+    @GetMapping("/api/v1/members/{member_id}")
+    public ResponseEntity<Member> showMember(@PathVariable int member_id){
+        Member member = memberService.findById(member_id);
+
+        if(member == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(member);
+    }
+
+    // 개인 정보 수정
+    // uuid, 이름, 생년월일, 전화번호, 학번, 프로필 사진
+    @PatchMapping("/api/v1/members/{member_id}")
+    public ResponseEntity<Member> updateMember(@PathVariable int member_id, @RequestBody MemberRequestDto memberRequestDto) {
+        try {
+            memberService.update(member_id, memberRequestDto);
+            return ResponseEntity.ok().build();
+        }catch(Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }

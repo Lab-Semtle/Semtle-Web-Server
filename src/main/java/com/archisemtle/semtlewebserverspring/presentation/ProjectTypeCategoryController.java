fe5565badc19927987e54ea0908d809a3595d6e1
Leeforgiveness <grang0224@g.kmou.ac.kr>
2025-01-10 16:07:26 +0900

    feat: requestMapping 주소 변경(#11)

--- src/main/java/com/archisemtle/semtlewebserverspring/presentation/ProjectTypeCategoryController.java
+++ src/main/java/com/archisemtle/semtlewebserverspring/presentation/ProjectTypeCategoryController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/projectTypeCategory")
+@RequestMapping("/api/v1/projecttypecategory")
 public class ProjectTypeCategoryController {
 
     private final ProjectTypeCategoryService projectTypeCategoryService;

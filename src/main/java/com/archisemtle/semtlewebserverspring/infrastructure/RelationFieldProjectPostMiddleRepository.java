f118d95eaffadaff0a43be9a8af0df23976a7f40
Leeforgiveness <grang0224@g.kmou.ac.kr>
2025-01-12 23:48:46 +0900

    feat: 게시물 상세 삭제 기능 구현(#11)

--- src/main/java/com/archisemtle/semtlewebserverspring/infrastructure/RelationFieldProjectPostMiddleRepository.java
+++ src/main/java/com/archisemtle/semtlewebserverspring/infrastructure/RelationFieldProjectPostMiddleRepository.java
@@ -2,10 +2,12 @@ package com.archisemtle.semtlewebserverspring.infrastructure;
 
 import com.archisemtle.semtlewebserverspring.domain.RelationFieldProjectPostMiddle;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RelationFieldProjectPostMiddleRepository extends
     JpaRepository<RelationFieldProjectPostMiddle, Long> {
 
-    List<RelationFieldProjectPostMiddle> findAllByProjectBoardId(Long id);
+    List<RelationFieldProjectPostMiddle> findAllByProjectBoardId(Long projectBoardId);
+    void deleteAllByProjectBoardId(Long projectBoardId);
 }

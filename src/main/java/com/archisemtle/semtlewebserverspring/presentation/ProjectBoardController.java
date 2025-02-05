940e15368964b3865afda0b70657f2097ff32123
Leeforgiveness <grang0224@g.kmou.ac.kr>
2025-02-06 01:47:35 +0900

    feat: 게시물 리스트 조회 완성(#11)

--- src/main/java/com/archisemtle/semtlewebserverspring/presentation/ProjectBoardController.java
+++ src/main/java/com/archisemtle/semtlewebserverspring/presentation/ProjectBoardController.java
@@ -3,17 +3,21 @@ package com.archisemtle.semtlewebserverspring.presentation;
 import com.archisemtle.semtlewebserverspring.application.ProjectBoardService;
 import com.archisemtle.semtlewebserverspring.common.CommonResponse;
 import com.archisemtle.semtlewebserverspring.dto.ProjectBoardResponseDto;
+import com.archisemtle.semtlewebserverspring.dto.ProjectListRequestDto;
 import com.archisemtle.semtlewebserverspring.vo.AddProjcetBoardRequestVo;
 import com.archisemtle.semtlewebserverspring.vo.ProjectBoardResponseVo;
 import com.archisemtle.semtlewebserverspring.vo.UpdateProjectBoardRequestVo;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -47,11 +51,21 @@ public class ProjectBoardController {
     }
 
     @PostMapping("/update/{projectBoardId}")
-    public CommonResponse<String> UpdateProjectBoard(@PathVariable("projectBoardId") Long id,
+    public CommonResponse<String> updateProjectBoard(@PathVariable("projectBoardId") Long id,
         @RequestBody
         UpdateProjectBoardRequestVo updateProjectBoardRequestVo) {
         projectBoardService.updateProjectBoard(id,
             UpdateProjectBoardRequestVo.voToDto(updateProjectBoardRequestVo));
         return CommonResponse.success("게시물 수정 성공");
+    }
+
+    @GetMapping("/projectboardlist")
+    public CommonResponse<Page<ProjectListRequestDto>> getProjectBoardList(
+        @RequestParam(name = "page", defaultValue = "0") int page,
+        @RequestParam(name = "size", defaultValue = "10") int size) {
+        Page<ProjectListRequestDto> projectBoardList = projectBoardService.getProjectBoardList(page,
+            size);
+
+        return CommonResponse.success("게시물 리스트", projectBoardList);
     }
 }
